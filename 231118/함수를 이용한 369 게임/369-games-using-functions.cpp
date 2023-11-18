9266d82738b046b61637a42379e61ca76d629447
Zzangtae <61003779+jongtae0509@users.noreply.github.com>
2023-11-18 17:25:31 +0900

    Title: 함수를 이용한 369 게임, Time: 9ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 231118/함수를 이용한 369 게임/369-games-using-functions.cpp
+++ 231118/함수를 이용한 369 게임/369-games-using-functions.cpp
@@ -6,10 +6,12 @@ int main() {
     int s=0;
     for(int i=a;i<=b;i++){
         int j=i;
+        if(i%3==0){
+            s++;
+            continue;
+        }
         while(j>0){
-            if((j%10)%3==0){
-                s++;
-            }
+            if((j%10)%3==0) s++;
             j/=10;
         }
     }
