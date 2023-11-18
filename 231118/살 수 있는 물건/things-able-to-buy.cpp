ef07703509649813830feeaeca0ff146d8f201a7
Zzangtae <61003779+jongtae0509@users.noreply.github.com>
2023-11-18 16:40:16 +0900

    Title: 살 수 있는 물건, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231118/살 수 있는 물건/things-able-to-buy.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n; cin >> n;
+    if(n>=3000) cout << "book";
+    else if(n>=1000) cout << "mask";
+    else cout << "no";
+}
