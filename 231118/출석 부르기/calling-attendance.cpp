2e2de1571f8ac329288ddea8dbf0c5c8dc5aaae3
Zzangtae <61003779+jongtae0509@users.noreply.github.com>
2023-11-18 16:42:15 +0900

    Title: 출석 부르기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231118/출석 부르기/calling-attendance.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n; cin >> n;
+    switch (n){
+        case 1:
+            cout <<"John";
+            break;
+        case 2:
+            cout << "Tom";
+            break;
+        case 3:
+            cout << "Paul";
+            break;
+    }
+}
