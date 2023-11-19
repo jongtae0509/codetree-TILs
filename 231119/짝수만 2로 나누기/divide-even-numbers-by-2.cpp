8d21924e94c586b3f7f52357c9a8ad738770b8fe
Zzangtae <61003779+jongtae0509@users.noreply.github.com>
2023-11-19 23:38:04 +0900

    Title: 짝수만 2로 나누기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231119/짝수만 2로 나누기/divide-even-numbers-by-2.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int n; cin >> n;
+    for(int i=0;i<n;i++){
+        int t; cin >> t;
+        if(t%2==0) t/=2;
+        else cout << t << " ";
+    }
+}
