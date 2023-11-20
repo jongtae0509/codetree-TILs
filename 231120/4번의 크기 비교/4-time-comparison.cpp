159c00745f82670ff93693eea05231c1d6feb699
Zzangtae <61003779+jongtae0509@users.noreply.github.com>
2023-11-20 16:41:15 +0900

    Title: 4번의 크기 비교, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231120/4번의 크기 비교/4-time-comparison.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int a; cin >> a;
+    for(int i=0;i<4;i++){
+        int t; cin >> t;
+        if(a>t) puts("1");
+        else puts("0");
+    }
+}
