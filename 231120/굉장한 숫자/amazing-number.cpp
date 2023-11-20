3caf71de717f554beca07dd2acf6fd23eac6de57
Zzangtae <61003779+jongtae0509@users.noreply.github.com>
2023-11-20 16:50:57 +0900

    Title: 굉장한 숫자, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 231120/굉장한 숫자/amazing-number.cpp
+++ 231120/굉장한 숫자/amazing-number.cpp
@@ -2,6 +2,6 @@
 using namespace std;
 int main(){
     int n; cin >> n;
-    if((n%2==0&&n%5==0)||(n%2==1&&n%3==0)) cout << "true;
+    if((n%2==0&&n%5==0)||(n%2==1&&n%3==0)) cout << "true";
     else cout << "false";
 }
