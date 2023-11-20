fde8ad7470cb2ca4a311bf2a517f35de910ebd80
Zzangtae <61003779+jongtae0509@users.noreply.github.com>
2023-11-20 16:40:22 +0900

    Title: 4가지 관계연산자, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231120/4가지 관계연산자/four-relational-operators.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int a,b; cin >> a >> b;
+    if(a>=b) puts("1");
+    else puts("0");
+    if(a>b) puts("1");
+    else puts("0");
+    if(b>=a) puts("1");
+    else puts("0");
+    if(b>a) puts("1");
+    else puts("0");
+}
