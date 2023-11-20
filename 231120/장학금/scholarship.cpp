8aa36e65d395af55dded08c8174c1565e73c0089
Zzangtae <61003779+jongtae0509@users.noreply.github.com>
2023-11-20 16:55:29 +0900

    Title: 장학금, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 231120/장학금/scholarship.cpp
+++ 231120/장학금/scholarship.cpp
@@ -3,9 +3,9 @@ using namespace std;
 int main(){
     int a,b; cin >> a >> b;
     int sum=0;
-    if(a>=95) sum+=10;
-    else if(a>=90) sum+=5;
-    if(b>=95) sum+=10;
-    else if(b>=90) sum+=5;
+    if(a>=90){
+        if(b>=95) sum+=100000;
+        else if(b>=90) sum+=50000;
+    }
     cout << sum;
 }
