ee3352861258670ccfbe3fd3ed98ce78c85a2e5f
Zzangtae <61003779+jongtae0509@users.noreply.github.com>
2023-11-20 16:57:26 +0900

    Title: 좀 더 어려운 수학 점수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231120/좀 더 어려운 수학 점수/math-scores-are-more-difficult.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int a,b,c,d; cin >> a >> b >> c >> d;
+    if(a>c) cout << "A";
+    else if(c>a) cout << "B";
+    else{
+        if(b>d) cout << "A";
+        else cout << "B";
+    }
+}
