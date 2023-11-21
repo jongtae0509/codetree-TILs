b30196160b98390ce6554d0d2bc2f94f1316a336
Zzangtae <61003779+jongtae0509@users.noreply.github.com>
2023-11-21 22:41:40 +0900

    Title: 남녀노소 구분짓기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 231121/남녀노소 구분짓기/sex-and-age.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a,b; cin >> a >> b;
+    if(a==0){
+        if(b>=19) puts("MAN");
+        else puts("BOY");
+    }
+    else{
+        if(b>=19) puts("WOMAN;
+        else puts("GIRL");
+    }
+}
