f51e8a96c0fb39f4f5512007880b5d43bbfc2b00
Zzangtae <61003779+jongtae0509@users.noreply.github.com>
2023-11-22 23:54:34 +0900

    Title: 높은 수에서 낮은 수까지, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231122/높은 수에서 낮은 수까지/from-high-to-low.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a,b; cin >> a >> b;
+    if(a>b) for(int i=a;i>=b;i--) cout << i << ' ';
+    else for(int i=b;i>a;i--) cout << i << ' ';
+}
