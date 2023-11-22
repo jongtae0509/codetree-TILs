420bae2aa87dcd5452218d447eb2a3554f886003
Zzangtae <61003779+jongtae0509@users.noreply.github.com>
2023-11-23 00:05:39 +0900

    Title: 배수의 개수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 231123/배수의 개수/number-of-multipliers.cpp
+++ 231123/배수의 개수/number-of-multipliers.cpp
@@ -2,10 +2,12 @@
 using namespace std;
 
 int main() {
-    int cnt=0;
+    int cnt3=0;
+    int cnt5=0;
     for(int i=0;i<10;i++){
         int t; cin >> t;
-        if(t%3==0||t%5==0) cnt++;
+        if(t%3==0) cnt3++;
+        if(t%5==0) cnt5++;
     }
-    cout << cnt;
+    cout << cnt3 << ' ' << cnt5;
 }
