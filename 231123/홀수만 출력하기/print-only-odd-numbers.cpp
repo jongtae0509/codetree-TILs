3caef598d78149e3147c2f4c97eb0efb29b75c11
Zzangtae <61003779+jongtae0509@users.noreply.github.com>
2023-11-23 00:01:38 +0900

    Title: 홀수만 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 231123/홀수만 출력하기/print-only-odd-numbers.cpp
+++ 231123/홀수만 출력하기/print-only-odd-numbers.cpp
@@ -5,6 +5,6 @@ int main() {
     int n; cin >> n;
     for(int i=0;i<n;i++){
         int t; cin >> t;
-        if(t%3==0&&i%2==1) cout << t << '\n';
+        if(t%3==0&&t%2==1) cout << t << '\n';
     }
 }
