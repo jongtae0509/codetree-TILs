1c126d603bdd3e35911b64e7cda925bbbeb3910d
Zzangtae <61003779+jongtae0509@users.noreply.github.com>
2023-11-24 23:13:55 +0900

    Title: 1부터의 합, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 231124/1부터의 합/sum-from-the-one.cpp
+++ 231124/1부터의 합/sum-from-the-one.cpp
@@ -1,10 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main() {
-   int n,i=0,sum=0; cin >> n;
-   while(sum<n){
-    i++;
+   int n; cin >> n;
+   int sum=0;
+   for(int i=1;i<=n;i++){
     sum+=i;
+    if(sum>=n) break;
    }
-   cout << sum-i;
+   cout << sum;
 }
