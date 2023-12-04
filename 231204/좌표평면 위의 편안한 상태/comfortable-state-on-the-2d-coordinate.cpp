e2246c7b9d3f1782d049ec136fdf18e5a7fd63db
Zzangtae <61003779+jongtae0509@users.noreply.github.com>
2023-12-04 19:58:11 +0900

    Title: 좌표평면 위의 편안한 상태, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231204/좌표평면 위의 편안한 상태/comfortable-state-on-the-2d-coordinate.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+int dx[4]={0,1,0,-1};
+int dy[4]={1,0,-1,0};
+int arr[2000][2000];
+
+int check(int x,int y){
+    int sum=0;
+    for(int dir=0;dir<4;dir++){
+        int nx=x+dx[dir];
+        int ny=y+dy[dir];
+        if(nx<0||ny<0) continue;
+        if(arr[nx][ny]==1) sum++;
+    }
+    return sum;
+}
+int main() {
+    //점을 찍었을 때 그 점의 범위 안에 있는 애들만 검색 3개 넘은애들은 --
+    int n; cin >> n;
+    queue<pair<int,int>> q;
+    int aa=0;
+    //for(int i=0;i<n;i++) for(int j=0;j<n;j++) arr[i][j]=-1;
+    for(int i=0;i<n;i++){
+        int x,y; cin >> y >> x;
+        arr[x][y]=1;    //점을 찍었어요
+        for(int dir=0;dir<4;dir++){
+            int nx=x+dx[dir];
+            int ny=y+dy[dir];
+            if(check(nx,ny)==3) aa++;
+            else if(check(nx,ny)>3) aa--;
+        }
+        cout << aa << '\n';
+    }
+}
