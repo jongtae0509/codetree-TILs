dc8a21dbfc7582af9268f6b1a863ae517b87763a
Zzangtae <61003779+jongtae0509@users.noreply.github.com>
2023-12-04 19:39:32 +0900

    Title: 격자 늘리기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 231204/격자 늘리기/stretch-grid.cpp
+++ 231204/격자 늘리기/stretch-grid.cpp
@@ -2,24 +2,20 @@
 using namespace std;
 int main() {
     int n,m,k; cin >> n >> m >> k;
-    char c[10][10];
+    char c[100][100];
     for(int i=0;i<n;i++){
         for(int j=0;j<m;j++){
             cin >> c[i][j];
         }
     }
     for(int i=0;i<n;i++){
-        for(int j=0;j<m;j++){
-            for(int t=0;t<k;t++){
-                cout << c[i][j];
+        for(int z=0;z<k;z++){
+            for(int j=0;j<m;j++){
+                for(int t=0;t<k;t++){
+                    cout << c[i][j];
+                }
             }
+            cout << '\n';
         }
-        cout << '\n';
-        for(int j=0;j<m;j++){
-            for(int t=0;t<k;t++){
-                cout << c[i][j];
-            }
-        }
-        cout << '\n';
     }
 }
